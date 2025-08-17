d150192769ed59de4a8277ca58a55bca32442864
SangWon7242 <gotkdqja@gmail.com>
2025-08-17 11:46:31 +0900

    home/increase, 인스턴스 변수의 수명 관리

--- src/main/java/com/sbs/basic1/boundedContext/home/controller/HomeController.java
+++ src/main/java/com/sbs/basic1/boundedContext/home/controller/HomeController.java
@@ -6,6 +6,13 @@ import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller // 스프링부트에게 해당 클래스는 컨트롤러라고 명시
 public class HomeController {
+  private int id;
+
+  public HomeController() {
+    id = -1;
+  }
+
+
   @GetMapping("/home/main")
   // http://localhost:8080/home/main 요청이 GET 요청으로 들어옴
   // 아래 메서드를 실행
@@ -24,5 +31,24 @@ public class HomeController {
   @ResponseBody
   public String showMain3() {
     return "스프링부트는 재밌습니까?";
+  }
+
+  @GetMapping("/home/increase")
+  @ResponseBody // 데이터를 담아 응답에 보냄, 보낸 데이터는 모두 문자열이다.
+  public int showIncrease() {
+    return ++id;
+  }
+
+  @GetMapping("/home/decrease")
+  @ResponseBody // 데이터를 담아 응답에 보냄, 보낸 데이터는 모두 문자열이다.
+  public int showDecrease() {
+    return --id;
+  }
+
+  @GetMapping("/home/initNumber")
+  @ResponseBody // 데이터를 담아 응답에 보냄, 보낸 데이터는 모두 문자열이다.
+  public int showInitNumber() {
+    id = 0;
+    return id;
   }
 }
